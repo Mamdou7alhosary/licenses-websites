8c0b4ff6b97f43cb995fefbb92e7f9f6a10bfabe
mamdouh <mamdou7alhosary@gmail.com>
2025-01-10 02:18:39 +0100

    Initial commit

---
+++ src/LicenseCheck.php
@@ -0,0 +1,27 @@
+<?php
+
+
+
+namespace Vendor\LicenseProtection;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class LicenseCheck {
+    public function handle(Request $request, Closure $next) {
+        $domain = $request->getHost();
+        $licenseKey = env('LICENSE_KEY');
+
+        $response = Http::get('https://icrm.com.tr/api/check-license', [
+            'domain' => $domain,
+            'license_key' => $licenseKey
+        ]);
+
+        if (!$response->json()['valid']) {
+            abort(403, '🚨 الترخيص غير صالح 🚨');
+        }
+
+        return $next($request);
+    }
+}
